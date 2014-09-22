0780885949ad0fd4a2e492725d2676c6aee61435
rgirish2 <rgirish2@siebl-0220-17.ews.illinois.edu>
2014-09-22 03:31:15 -0500

    User signin, signout, signup completed. List do data is static. Need to update that.

--- js/nextCapital.js
+++ js/nextCapital.js
@@ -1,25 +1,82 @@
+"use strict"
+
 var user_email;
 var user_id;
 var user_token;
 var user_todos = [];
 
-function getRecords (event) {
-	var emailVal = $("#email").val();
-	var pwd = $("#password").val();
+function signUp (event) {
+	var signUpEmailVal = $("#signUpEmail").val();
+	var signUpPwd = $("#signUpPassword").val();
     $.ajax ({
 		url : "http://recruiting-api.nextcapital.com/users",
 		type : "POST",
-		data : {email : emailVal, password : pwd},
+		data : {email : signUpEmailVal, password : signUpPwd},
 		crossDomain : true,
 		datatype : "jsonp",
 		success : function (response) {
 					assignValues(response);
-				},
+		},
 		error : function (response) {
-					alert(response.email);
 					alert("Error in request. Please try again.");				
-				}
+		}
 	});
+};
+
+function logoutCurrentUser(event) {
+	$.ajax({
+		url : "http://recruiting-api.nextcapital.com/users/sign_out",
+		type : "DELETE",
+		data : {api_token : user_token, user_id : user_id},
+		crossDomain : true,
+		datatype : "jsonp",
+		success : function () {
+					logOutCleanUp();
+		},
+		error : function () {
+					alert("Issues with logging out. Removing all current data. Please log-in again.");
+					logOutCleanUp();
+		}
+		
+	});	
+};
+
+function logOutCleanUp () {
+	user_email = null;
+	user_id = null;
+	user_token = null;
+	user_todos = null;
+
+	$("#listsGoHereDiv").empty();	
+	$("#listsGoHereDiv").hide();
+	$("#logOutButton").hide();
+	$("#writeAndButtonsDiv").show();
+};
+
+function signIn (event) {
+	var signInEmailVal = $("#signInEmail").val();
+	var signInPwd = $("#signInPassword").val();
+	$.ajax ({
+		url : "http://recruiting-api.nextcapital.com/users/sign_in",
+		type : "POST",
+		data : {email : signInEmailVal, password : signInPwd},
+		crossDomain : true,
+		datatype : "jsonp",
+		success : function (response) {
+					assignValues(response);
+		},
+		error : function (response) {
+					alert("Error signing in. Please try again.");
+		}
+	});
+};
+
+function deleteToDo(id) {
+
+};
+
+function updateToDo(id) {
+
 };
 
 function assignValues (result) {
@@ -32,7 +89,13 @@ function assignValues (result) {
 };
 
 function showDetails () {
-	$("#coreDiv").empty();
-	$("#coreDiv").append("<p>" + user_email + "</p><p>" + user_id + "</p><p>" + user_token + "</p>");
+	$("#writeAndButtonsDiv").hide();
+	$("#logOutButton").show();
+	$("#listsGoHereDiv").show();
 };
+
+$(function() {
+	$( "#sortable" ).sortable();
+	$( "#sortable" ).disableSelection();
+});
 
