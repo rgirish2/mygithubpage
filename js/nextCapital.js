609b7ce5d24faebf95e3b72257cca7fd649eb3b3
rgirish2 <rgirish2@siebl-0403-26.ews.illinois.edu>
2014-09-22 21:22:05 -0500

    All the functionality done. Minor UI clean up required.

--- js/nextCapital.js
+++ js/nextCapital.js
@@ -1,9 +1,10 @@
-"use strict"
+"use strict";
 
-var user_email;
-var user_id;
-var user_token;
+var user_email = null;
+var user_id = null;
+var user_token = null;
 var user_todos = [];
+var user_todosIDs = [];
 
 function signUp (event) {
 	var signUpEmailVal = $("#signUpEmail").val();
@@ -23,36 +24,6 @@ function signUp (event) {
 	});
 };
 
-function logoutCurrentUser(event) {
-	$.ajax({
-		url : "http://recruiting-api.nextcapital.com/users/sign_out",
-		type : "DELETE",
-		data : {api_token : user_token, user_id : user_id},
-		crossDomain : true,
-		datatype : "jsonp",
-		success : function () {
-					logOutCleanUp();
-		},
-		error : function () {
-					alert("Issues with logging out. Removing all current data. Please log-in again.");
-					logOutCleanUp();
-		}
-		
-	});	
-};
-
-function logOutCleanUp () {
-	user_email = null;
-	user_id = null;
-	user_token = null;
-	user_todos = null;
-
-	$("#listsGoHereDiv").empty();	
-	$("#listsGoHereDiv").hide();
-	$("#logOutButton").hide();
-	$("#writeAndButtonsDiv").show();
-};
-
 function signIn (event) {
 	var signInEmailVal = $("#signInEmail").val();
 	var signInPwd = $("#signInPassword").val();
@@ -63,35 +34,186 @@ function signIn (event) {
 		crossDomain : true,
 		datatype : "jsonp",
 		success : function (response) {
-					assignValues(response);
+            assignValues(response);
 		},
-		error : function (response) {
-					alert("Error signing in. Please try again.");
+		error : function (response, status, error) {
+            alert("Error signing in. Please try again.");
 		}
 	});
 };
 
-function deleteToDo(id) {
-
+function logoutCurrentUser(event) {
+	$.ajax({
+		url : "http://recruiting-api.nextcapital.com/users/sign_out",
+		type : "DELETE",
+		data : {api_token : user_token, user_id : user_id},
+		crossDomain : true,
+		datatype : "jsonp",
+		success : function () {
+            logOutCleanUp();
+		},
+		error : function () {
+            alert("Issues with logging out. Removing all current data. Please log-in again.");
+            logOutCleanUp();
+		}	
+	});	
 };
 
-function updateToDo(id) {
+function logOutCleanUp () {
+	user_email = null;
+	user_id = null;
+	user_token = null;
+	user_todos = null;
+	user_todosIDs = null;
 
+	$("#sortable").empty();	
+	$("#listsGoHereDiv").hide();
+	$("#logOutButton").hide();
+	$("#writeAndButtonsDiv").show();
+};
+
+function updateToDo(todoId) {
+    $("#updateTodoModalHeaderLabel").empty();
+    $("#updateTodoModalHeaderLabel").append(todoId);
+    
+    var updateTodoHTML = '<label class="col-sm-4 control-label" for="updateTodoDescription" style="color:black">Description</label><div class="col-sm-8"><textarea class="form-control" id="updateTodoDescription"></textarea></div><label class="col-sm-4 control-label" for="updateTodoIsComplete" style="color:black">is_complete: </label><div class="col-sm-8"><select class="form-control" id="updateTodoIsComplete"><option value=true>True</option><option value=false>False</option></select></div>';
+    
+    $("#todoIdContentGoesHere").empty();
+    $("#todoIdContentGoesHere").append(updateTodoHTML);
+        
+    $("#updateATodoModal").modal({
+        show : true
+    }); 
+};
+
+function submitUpdateTodoRequest() {
+    $("#updateATodoModal").modal({
+        show : false
+    });
+    
+    var newDescription = $("#updateTodoDescription").val();
+    var newIsCompleteValue = $("#updateTodoIsComplete").val();
+    var todoIdToUpdate = $("#updateTodoModalHeaderLabel").text();
+    
+    var todoUpdateURL = "http://recruiting-api.nextcapital.com/users/" + user_id + "/todos/" + todoIdToUpdate;
+    
+    $("#updateTodoModalHeaderLabel").empty();
+    $("#todoIdContentGoesHere").empty();
+    
+    $.ajax ({
+        url : todoUpdateURL,
+        type : "PUT",
+        data : {api_token : user_token, todo : {description : newDescription, is_complete : newIsCompleteValue}},
+        crossDomain : true,
+        datatypes : "jsonp",
+        success : function (response) {
+            updateATodoInList (todoIdToUpdate, response);
+            $("#sortable").empty();
+            showDetails();
+        },
+        error : function (response, status, error) {
+            alert("Todo cannot be update. Please try again.");           
+        }    
+    });
+};
+
+function updateATodoInList (todoIdToUpdate, newValueForThis) {
+    for (var i = user_todos.length - 1; i >= 0; i--) {
+        if (user_todos[i].id == todoIdToUpdate) {
+            user_todos.splice(i, 1);
+        }
+    }
+    
+    user_todos.push({
+        description : newValueForThis.description,
+        id : newValueForThis.id,
+        is_complete : newValueForThis.is_complete
+    });
+    return;    
+};
+
+function showCreateANewTodoModal() {
+    $("#addNewTodoModal").modal({
+        show : true
+    });
+};
+
+function submitCreateANewTodoRequest() {
+    $("#addNewTodoModal").modal({
+        show : false
+    });
+    
+    var descriptionOfNewTodo = $("#newTodoDescription").val();
+    var baseCreateANewTodoUrl = "http://recruiting-api.nextcapital.com/users/";
+    
+    $.ajax({
+        url : baseCreateANewTodoUrl + user_id + "/todos",
+        type : "POST",
+        data : {api_token : user_token, todo : {description : descriptionOfNewTodo}},
+        crossDomain : true,
+        datatypes : "jsonp",
+        success : function (response) {
+            user_todos.push({
+                description : response.description,
+                id : response.id,
+                is_complete : response.is_complete
+            });
+            $("#sortable").empty();
+            showDetails();
+        },
+        error : function (response, status, error) {
+            alert("Error adding a new todo. Please try again.")
+        }
+    });
+};
+
+function getUserTodos (id, token) {
+    $.ajax({
+        url : "http://recruiting-api.nextcapital.com/users/" + id + "/todos",
+        data : {api_token : token},
+        type : "GET",
+        crossDomain : true,
+        datatypes : "jsonp",
+        success : function (response) {
+            user_todos = response;
+            showDetails();
+        },
+        error : function (response, status, error) {
+            alert("Could not retrieve todos. Logout and try again.")
+        }
+    }); 
 };
 
 function assignValues (result) {
 	user_email = result.email;
 	user_id = result.id;
 	user_token = result.api_token;
-	user_todos = result.todos;
-	showDetails();
+	user_todosIDs = result.todos;
+    
+    getUserTodos(user_id, user_token);
 	return;
 };
 
 function showDetails () {
 	$("#writeAndButtonsDiv").hide();
+	listUserTodos();
 	$("#logOutButton").show();
 	$("#listsGoHereDiv").show();
+};
+
+function listUserTodos () {
+    var oneItemHTMLBefore = '<div id="col-sm-12" style="padding-bottom: 15px; padding-top:15px"><li class="ui-state-default" style="color:black"><span class="ui-icon ui-icon-arrowthick-2-n-s"></span>';
+    
+    var oneItemHTMLAfter = '<button id="btn btn-sm btn-primary" onClick="updateToDo(';
+    
+    var oneItemHTMLAfterAfter = ')">Update</button>></li></div>';
+    
+    if (user_todos !== null) {
+        for (var i = 0; i < user_todos.length; i++) {
+            var oneItemCompleteHTML = oneItemHTMLBefore + "Description: " + user_todos[i].description + " Id: " + user_todos[i].id + " is_complete: " + user_todos[i].is_complete + oneItemHTMLAfter + user_todos[i].id + oneItemHTMLAfterAfter;
+            $("#sortable").append(oneItemCompleteHTML);
+	    }
+	}
 };
 
 $(function() {
